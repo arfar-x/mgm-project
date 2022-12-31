44142c6cef88916b722c72a73fcd1c3a2cbfdae1
Alireza Farhanian <aariow01@gmail.com>
2022-12-31 21:56:58 +0330

    Correct product routes

--- app/Services/ProductService/Routes/api.php
+++ app/Services/ProductService/Routes/api.php
@@ -6,23 +6,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('api')->prefix('api')->group(function () {
 
-    Route::middleware('auth:sanctum')->prefix('admin')->name('admin.')->group(function () {
+    Route::middleware('auth:sanctum')->prefix('admin/products')->name('admin.products.')->group(function () {
 
-        Route::apiResource('products', ProductController::class);
-
-        Route::prefix('products')->name('products.')->group(function () {
-
-            Route::patch('/{product}/change-category', [ProductController::class, 'changeCategory'])->name('change-category');
-            Route::patch('/{product}/activate', [ProductController::class, 'activate'])->name('activate');
-            Route::patch('/{product}/deactivate', [ProductController::class, 'deactivate'])->name('deactivate');
-            Route::post('/{product}/upload', [ProductController::class, 'upload'])->name('upload');
-            Route::get('/{product}/tags', [ProductController::class, 'getTags'])->name('get-tags');
-            Route::patch('/{product}/tags/sync', [ProductController::class, 'syncTags'])->name('sync-tags');
-            Route::delete('/{product}/{uuid}', [ProductController::class, 'deleteFile'])->name('delete-file');
-            Route::patch('/{product}/{uuid}', [ProductController::class, 'setCover'])->name('set-cover');
-        });
-
-        Route::prefix('categories/products')->group(function () {
+        Route::prefix('/categories')->group(function () {
 
             Route::get('/', [CategoryController::class, 'index'])->name('index');
             Route::post('/', [CategoryController::class, 'store'])->name('store');
@@ -36,6 +22,22 @@ Route::middleware('api')->prefix('api')->group(function () {
             Route::delete('/{category}/{uuid}', [CategoryController::class, 'deleteFile'])->name('delete-file');
             Route::patch('/{category}/{uuid}', [CategoryController::class, 'setCover'])->name('set-cover');
         });
+
+        Route::get('/', [ProductController::class, 'index'])->name('index');
+        Route::post('/', [ProductController::class, 'store'])->name('store');
+        Route::get('/{product}', [ProductController::class, 'show'])->name('show');
+        Route::patch('/{product}', [ProductController::class, 'update'])->name('update');
+        Route::delete('/{product}', [ProductController::class, 'destroy'])->name('destroy');
+
+        Route::patch('/{product}/change-category', [ProductController::class, 'changeCategory'])->name('change-category');
+        Route::patch('/{product}/activate', [ProductController::class, 'activate'])->name('activate');
+        Route::patch('/{product}/deactivate', [ProductController::class, 'deactivate'])->name('deactivate');
+        Route::post('/{product}/upload', [ProductController::class, 'upload'])->name('upload');
+        Route::get('/{product}/tags', [ProductController::class, 'getTags'])->name('get-tags');
+        Route::patch('/{product}/tags/sync', [ProductController::class, 'syncTags'])->name('sync-tags');
+        Route::delete('/{product}/{uuid}', [ProductController::class, 'deleteFile'])->name('delete-file');
+        Route::patch('/{product}/{uuid}', [ProductController::class, 'setCover'])->name('set-cover');
+
     });
 
     Route::prefix('panel/products')->name('panel.products.')->group(function () {
