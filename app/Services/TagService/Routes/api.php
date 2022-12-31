ea13fcb8f66c7d0ff03d66257cb4ed797b199017
Alireza Farhanian <aariow01@gmail.com>
2022-12-31 22:07:56 +0330

    Correct tag routes

--- app/Services/TagService/Routes/api.php
+++ app/Services/TagService/Routes/api.php
@@ -5,15 +5,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('api')->prefix('api')->group(function () {
 
-    Route::middleware('auth:sanctum')->prefix('admin')->name('admin.')->group(function () {
+    Route::middleware('auth:sanctum')->prefix('admin/tags')->name('admin.tags.')->group(function () {
 
-        Route::apiResource('tags', TagController::class);
+        Route::get('/', [TagController::class, 'index'])->name('index');
+        Route::post('/', [TagController::class, 'store'])->name('store');
+        Route::get('/{tag}', [TagController::class, 'show'])->name('show');
+        Route::patch('/{tag}', [TagController::class, 'update'])->name('update');
+        Route::delete('/{tag}', [TagController::class, 'destroy'])->name('destroy');
 
-        Route::prefix('tags')->name('tags.')->group(function () {
-            Route::patch('/{tag}/activate', [TagController::class, 'activate'])->name('activate');
-            Route::patch('/{tag}/deactivate', [TagController::class, 'deactivate'])->name('deactivate');
-        });
-        
+        Route::patch('/{tag}/activate', [TagController::class, 'activate'])->name('activate');
+        Route::patch('/{tag}/deactivate', [TagController::class, 'deactivate'])->name('deactivate');
+
     });
 
     Route::prefix('panel/tags')->name('panel.tags.')->group(function () {
