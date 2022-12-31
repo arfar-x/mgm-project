ec9724f128fd5c0d2e2ae5db12ba3ed9dd43bfe2
Alireza Farhanian <aariow01@gmail.com>
2022-12-31 22:01:46 +0330

    Correct project routes

--- app/Services/ProjectService/Routes/api.php
+++ app/Services/ProjectService/Routes/api.php
@@ -6,23 +6,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('api')->prefix('api')->group(function () {
 
-    Route::middleware('auth:sanctum')->prefix('admin')->name('admin.')->group(function () {
+    Route::middleware('auth:sanctum')->prefix('admin/projects')->name('admin.projects.')->group(function () {
 
-        Route::apiResource('projects', ProjectController::class);
-
-        Route::prefix('projects')->name('projects.')->group(function () {
-
-            Route::patch('/{project}/change-category', [ProjectController::class, 'changeCategory'])->name('change-category');
-            Route::patch('/{project}/activate', [ProjectController::class, 'activate'])->name('activate');
-            Route::patch('/{project}/deactivate', [ProjectController::class, 'deactivate'])->name('deactivate');
-            Route::post('/{project}/upload', [ProjectController::class, 'upload'])->name('upload');
-            Route::get('/{project}/tags', [ProjectController::class, 'getTags'])->name('get-tags');
-            Route::patch('/{project}/tags/sync', [ProjectController::class, 'syncTags'])->name('sync-tags');
-            Route::delete('/{project}/{uuid}', [ProjectController::class, 'deleteFile'])->name('delete-file');
-            Route::patch('/{project}/{uuid}', [ProjectController::class, 'setCover'])->name('set-cover');
-        });
-
-        Route::prefix('categories/projects')->group(function () {
+        Route::prefix('/categories')->group(function () {
 
             Route::get('/', [CategoryController::class, 'index'])->name('index');
             Route::post('/', [CategoryController::class, 'store'])->name('store');
@@ -36,6 +22,22 @@ Route::middleware('api')->prefix('api')->group(function () {
             Route::delete('/{category}/{uuid}', [CategoryController::class, 'deleteFile'])->name('delete-file');
             Route::patch('/{category}/{uuid}', [CategoryController::class, 'setCover'])->name('set-cover');
         });
+
+        Route::get('/', [ProjectController::class, 'index'])->name('index');
+        Route::post('/', [ProjectController::class, 'store'])->name('store');
+        Route::get('/{project}', [ProjectController::class, 'show'])->name('show');
+        Route::patch('/{project}', [ProjectController::class, 'update'])->name('update');
+        Route::delete('/{project}', [ProjectController::class, 'destroy'])->name('destroy');
+
+        Route::patch('/{project}/change-category', [ProjectController::class, 'changeCategory'])->name('change-category');
+        Route::patch('/{project}/activate', [ProjectController::class, 'activate'])->name('activate');
+        Route::patch('/{project}/deactivate', [ProjectController::class, 'deactivate'])->name('deactivate');
+        Route::post('/{project}/upload', [ProjectController::class, 'upload'])->name('upload');
+        Route::get('/{project}/tags', [ProjectController::class, 'getTags'])->name('get-tags');
+        Route::patch('/{project}/tags/sync', [ProjectController::class, 'syncTags'])->name('sync-tags');
+        Route::delete('/{project}/{uuid}', [ProjectController::class, 'deleteFile'])->name('delete-file');
+        Route::patch('/{project}/{uuid}', [ProjectController::class, 'setCover'])->name('set-cover');
+
     });
 
     Route::prefix('panel/projects')->name('panel.projects.')->group(function () {
