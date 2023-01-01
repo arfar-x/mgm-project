396ff4792b60642685b9b9534620b3eff19cc5c2
Alireza Farhanian <aariow01@gmail.com>
2023-01-01 16:40:35 +0330

    Fix response service return types

--- app/Services/ResponseService/Lang/en/default.php
+++ app/Services/ResponseService/Lang/en/default.php
@@ -3,14 +3,14 @@
 return [
     'actions' => [
         'success' => 'Operation is done successfully.',
-        'retrieved' => 'Information retrieved succesfully.',
+        'retrieved' => 'Information retrieved successfully.',
         'info' => 'Information is provided to show.',
         'created' => 'The item is created successfully.',
         'updated' => 'The item is updated successfully.',
         'deleted' => 'The item is deleted successfully,',
         'no-content' => 'There is no content to show.',
         'unauthorized' => 'Please, login to your account first.',
-        'error' => 'There is an error while doing the opration.',
+        'error' => 'There is an error while doing the operation.',
         'not-found' => 'The item could not be found.',
     ]
 ];
