cfddc288af9fdb5cb4c5fc52d65cc14925f31162
Alireza Farhanian <aariow01@gmail.com>
2023-01-01 16:00:45 +0330

    Fix customer service return types

--- app/Services/CustomerService/Resources/CustomerCollection.php
+++ app/Services/CustomerService/Resources/CustomerCollection.php
@@ -3,13 +3,14 @@
 namespace App\Services\CustomerService\Resources;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Support\Collection;
 
 class CustomerCollection extends ResourceCollection
 {
     /**
      * The mapped collection instance.
      *
-     * @var \Illuminate\Support\Collection
+     * @var Collection
      */
     public $collection = CustomerResource::class;
 }
